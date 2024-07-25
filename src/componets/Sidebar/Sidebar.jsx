d114f29aefdf30e771958df09d262f52f32bdc2c
TIMMUZY <tynchtykergesh02@gmail.com>
2024-07-25 17:53:42 +0600

    ref: Refaring Sidebar panel

--- src/componets/Sidebar/Sidebar.jsx
+++ src/componets/Sidebar/Sidebar.jsx
@@ -1,11 +1,26 @@
 import Menu from '../Menu/Menu'
 import classes from '../Sidebar/Sidebar.module.scss'
+import profil from "./assets/images/proff.jpeg"
 
 const Sidebar = () => {
   return (
     <div className={classes.sidebar}>
-      <Menu to="/" label="Приборная панель" />
-      <Menu to="/driver" label="Водители" />
+      <div className={classes.profil}>
+        <div className={classes.flex}>
+          <div className={classes.circle}>
+            <img src={profil} alt='profile' />
+          </div>
+          <div className={classes.profilInfo}>
+            <p>Иванов Иван Иванович</p>
+            <p className={classes.phone}>+996774140282</p>
+            <p className={classes.status}>Старший Оператор</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Menu to='/driver' label='Водители' />
+        <Menu to='/order' label='Заказы' />
+      </div>
     </div>
   )
 }
