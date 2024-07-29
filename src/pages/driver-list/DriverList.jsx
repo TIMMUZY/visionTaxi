86a1c7679f9f4c497382a4f34752103fbb15c1d2
TIMMUZY <tynchtykergesh02@gmail.com>
2024-07-29 14:50:24 +0600

    pagination in DriverList page

--- src/pages/driver-list/DriverList.jsx
+++ src/pages/driver-list/DriverList.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import classes from './DriverList.module.scss';
 import { Pagination } from 'antd';
-import Search from "./icons/Search";
-// import Driver from './driver.json'
+import drivers from './driver.json';
 
 const DriverList = () => {
-  const [current, setCurrent] = useState(3);
+  const [current, setCurrent] = useState(1);
+  const pageSize = 12;
 
   const onChange = (page) => {
     console.log(page);
     setCurrent(page);
   };
+
+  const startIndex = (current - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const currentDrivers = drivers.slice(startIndex, endIndex);
 
   return (
     <div>
@@ -20,7 +24,11 @@ const DriverList = () => {
             <div className={classes.search}>
               <input type="text" className={classes.searchInput} placeholder="Search by Name" />
               <button className={classes.searchButton}>
-                <Search />
+                <svg className={classes.searchIcon} aria-hidden="true" viewBox="0 0 24 24">
+                  <g>
+                    <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+                  </g>
+                </svg>
               </button>
             </div>
           </li>
@@ -60,85 +68,31 @@ const DriverList = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className={classes.bodyCell}>134567431234</td>
-              <td className={classes.bodyCell}>
-                Иван Иванов <br /> Иванович
-              </td>
-              <td className={classes.bodyCell}>996134567431234</td>
-              <td className={classes.bodyCell}>O324OD</td>
-              <td className={classes.bodyCell}>BMW X5</td>
-              <td className={classes.bodyTime}>
-                <div className={classes.timeContent}>7 часов назад</div>
-              </td>
-            </tr>
-            <tr>
-              <td className={classes.bodyCell}>134567431234</td>
-              <td className={classes.bodyCell}>
-                Иван Иванов <br /> Иванович
-              </td>
-              <td className={classes.bodyCell}>996134567431234</td>
-              <td className={classes.bodyCell}>O324OD</td>
-              <td className={classes.bodyCell}>BMW X5</td>
-              <td className={classes.bodyTime}>
-                <div className={classes.timeContent}>7 часов назад</div>
-              </td>
-            </tr>
-            <tr>
-              <td className={classes.bodyCell}>134567431234</td>
-              <td className={classes.bodyCell}>
-                Иван Иванов <br /> Иванович
-              </td>
-              <td className={classes.bodyCell}>996134567431234</td>
-              <td className={classes.bodyCell}>O324OD</td>
-              <td className={classes.bodyCell}>BMW X5</td>
-              <td className={classes.bodyTime}>
-                <div className={classes.timeContent}>7 часов назад</div>
-              </td>
-            </tr>
-            <tr>
-              <td className={classes.bodyCell}>134567431234</td>
-              <td className={classes.bodyCell}>
-                Иван Иванов <br /> Иванович
-              </td>
-              <td className={classes.bodyCell}>996134567431234</td>
-              <td className={classes.bodyCell}>O324OD</td>
-              <td className={classes.bodyCell}>BMW X5</td>
-              <td className={classes.bodyTime}>
-                <div className={classes.timeContent}>7 часов назад</div>
-              </td>
-            </tr>
-            <tr>
-              <td className={classes.bodyCell}>134567431234</td>
-              <td className={classes.bodyCell}>
-                Иван Иванов <br /> Иванович
-              </td>
-              <td className={classes.bodyCell}>996134567431234</td>
-              <td className={classes.bodyCell}>O324OD</td>
-              <td className={classes.bodyCell}>BMW X5</td>
-              <td className={classes.bodyTime}>
-                <div className={classes.timeContent}>7 часов назад</div>
-              </td>
-            </tr>
-            <tr>
-              <td className={classes.bodyCell}>134567431234</td>
-              <td className={classes.bodyCell}>
-                Иван Иванов <br /> Иванович
-              </td>
-              <td className={classes.bodyCell}>996134567431234</td>
-              <td className={classes.bodyCell}>O324OD</td>
-              <td className={classes.bodyCell}>BMW X5</td>
-              <td className={classes.bodyTime}>
-                <div className={classes.timeContent}>7 часов назад</div>
-              </td>
-            </tr>
+            {currentDrivers.map((driver) => (
+              <tr key={driver.id}>
+                <td className={classes.bodyCell}>{driver.id}</td>
+                <td className={classes.bodyCell}>
+                  {driver.name} <br /> {driver.patronymic}
+                </td>
+                <td className={classes.bodyCell}>{driver.phone}</td>
+                <td className={classes.bodyCell}>{driver.carNumber}</td>
+                <td className={classes.bodyCell}>{driver.carModel}</td>
+                <td className={classes.bodyTime}>
+                  <div className={classes.timeContent}>{driver.lastActive}</div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
-        <div className={classes.line}>
-
-        </div>
+        <div className={classes.line}></div>
       </div>
-      <Pagination className={classes.Paginate} current={current} onChange={onChange} total={20} />
+      <Pagination
+        className={classes.Paginate}
+        current={current}
+        onChange={onChange}
+        pageSize={pageSize}
+        total={drivers.length}
+      />
     </div>
   );
 };
