2a379698be4653f354bdecdc942b4607694facdd
TIMMUZY <tynchtykergesh02@gmail.com>
2024-07-29 17:26:27 +0600

    ref: SideBar component

--- src/componets/Menu/Menu.jsx
+++ src/componets/Menu/Menu.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import classes from './Menu.module.scss'
 
-const MenuItem = ({ to, label }) => {
+const MenuItem = ({ to, label, icon }) => {
   const location = useLocation()
   const isActive = location.pathname === to
 
   return (
     <div className={`${classes.menuItem} ${isActive ? classes.active : ''}`}>
       <Link to={to} className={isActive ? classes.activeLink : ''}>
+        <img  src={icon} alt='' />
         {label}
       </Link>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,9 @@ import Navbar from './NavBar/NavBar'
 import StartPage from './StartPage/StartPage'
 import DriverList from './driver-list/DriverList'
 import Order from "./Order/Order"
+import Information from "./Information/Information"
+import Map from "./Map/Map"
+import Connection from "./Connection/Connection"
+import Settings from "./Settings/Settings"
 
-export { Navbar, StartPage, DriverList, Order }
+export { Navbar, StartPage, DriverList, Order, Information, Map, Connection, Settings }
