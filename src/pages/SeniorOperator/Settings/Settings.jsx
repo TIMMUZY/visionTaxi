c86948dff790d9b1cf5f886cef649539242246a3
TIMMUZY <tynchtykergesh02@gmail.com>
2024-09-04 12:59:46 +0600

    change

--- src/pages/SeniorOperator/Settings/Settings.jsx
+++ src/pages/SeniorOperator/Settings/Settings.jsx
@@ -40,65 +40,67 @@ const Settings = () => {
   }
 
   return (
-    <div className={classes.container}>
-      <div className={classes.mainContent}>
-        <div className={classes.blockcontainer}>
-          <h3 className={classes.Importanttitle}>Управление пользователями</h3>
-          <div className={classes.block}>
-            <h3>Профиль</h3>
-            <div>
-              <p className={classes.checkprofill}>
-                {profile.name} {profile.surname} {profile.patronymic}
-              </p>
-              <img className={classes.check} src={cheks} alt='' onClick={handleIconClick} />
+    <>
+      <div className={classes.container}>
+        <div className={classes.mainContent}>
+          <div className={classes.blockcontainer}>
+            <h3 className={classes.Importanttitle}>Управление пользователями</h3>
+            <div className={classes.block}>
+              <h3>Профиль</h3>
+              <div>
+                <p className={classes.checkprofill}>
+                  {profile.name} {profile.surname} {profile.patronymic}
+                </p>
+                <img className={classes.check} src={cheks} alt='' onClick={handleIconClick} />
+              </div>
+              <div className={classes.line}></div>
             </div>
-            <div className={classes.line}></div>
-          </div>
-          <div className={classes.block}>
-            <h3>Почта</h3>
-            <div>
-              <h3 className={classes.title}>
-                Ваш адрес электронной почты:
-                <br /> {profile.email}
-              </h3>
-              <img className={classes.check} src={cheks} alt='' onClick={handleEmailClick} />
+            <div className={classes.block}>
+              <h3>Почта</h3>
+              <div>
+                <h3 className={classes.title}>
+                  Ваш адрес электронной почты:
+                  <br /> {profile.email}
+                </h3>
+                <img className={classes.check} src={cheks} alt='' onClick={handleEmailClick} />
+              </div>
+              <div className={classes.line}></div>
             </div>
-            <div className={classes.line}></div>
-          </div>
-          <div className={classes.block}>
-            <h3>Уведомления</h3>
-            <div>
-              <h3 className={classes.title}>Rateit будет отправлять вам уведомления</h3>
-              <img className={classes.check} src={cheks} alt='' onClick={handleNotificationClick} />
+            <div className={classes.block}>
+              <h3>Уведомления</h3>
+              <div>
+                <h3 className={classes.title}>Rateit будет отправлять вам уведомления</h3>
+                <img className={classes.check} src={cheks} alt='' onClick={handleNotificationClick} />
+              </div>
+              <div className={classes.line}></div>
             </div>
-            <div className={classes.line}></div>
-          </div>
-          <div className={classes.block}>
-            <h3>Deactive account</h3>
-            <div>
-              <h3 className={classes.title}>Если хотите удалить аккаунт</h3>
-              <button className={classes.btn}>Деактивация</button>
+            <div className={classes.block}>
+              <h3>Deactive account</h3>
+              <div>
+                <h3 className={classes.title}>Если хотите удалить аккаунт</h3>
+                <button className={classes.btn}>Деактивация</button>
+              </div>
+              <div className={classes.line}></div>
             </div>
-            <div className={classes.line}></div>
+            <div className={classes.blockDop} onClick={handleLogoutClick}>
+              <img className={classes.exits} src={exit} alt='' />
+              <h3 className={classes.titlel}>Выйти с аккаунта</h3>
+            </div>
+            <div className={classes.line1}></div>
           </div>
-          <div className={classes.blockDop} onClick={handleLogoutClick}>
-            <img className={classes.exits} src={exit} alt='' />
-            <h3 className={classes.titlel}>Выйти с аккаунта</h3>
-          </div>
-          <div className={classes.line1}></div>
         </div>
+        {showEmailPanel && (
+          <div className={classes.emailPanel}>
+            <Email currentEmail={profile.email} onUpdateEmail={handleUpdateEmail} />
+          </div>
+        )}
+        {showNotificationPanel && (
+          <div className={classes.notificationPanel}>
+            <Notification currentNotification={profile.notifications} onUpdateNotification={handleUpdateNotification} />
+          </div>
+        )}
       </div>
-      {showEmailPanel && (
-        <div className={classes.emailPanel}>
-          <Email currentEmail={profile.email} onUpdateEmail={handleUpdateEmail} />
-        </div>
-      )}
-      {showNotificationPanel && (
-        <div className={classes.notificationPanel}>
-          <Notification currentNotification={profile.notifications} onUpdateNotification={handleUpdateNotification} />
-        </div>
-      )}
-    </div>
+    </>
   )
 }
 
